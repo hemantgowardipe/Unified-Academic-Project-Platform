eb5536f69370d3c9951240b7c3af8b590e627e55
AniketDhakate007 <142479987+AniketDhakate007@users.noreply.github.com>
2025-08-22 15:22:36 +0530

    Update AuthServiceApplication.java

--- auth-service/src/main/java/com/UAPP/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/UAPP/auth_service/AuthServiceApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
 
 		dotenv.entries().forEach(entry ->
 				System.setProperty(entry.getKey(), entry.getValue())
