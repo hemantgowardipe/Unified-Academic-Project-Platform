48835421021f4cfb93419aefea3f9ce77b1f6ad0
AniketDhakate007 <142479987+AniketDhakate007@users.noreply.github.com>
2025-08-22 14:44:21 +0530

    Update SubmissionServiceApplication.java

--- submissionService/src/main/java/com/UAPP/submissionService/SubmissionServiceApplication.java
+++ submissionService/src/main/java/com/UAPP/submissionService/SubmissionServiceApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SubmissionServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
 
 		dotenv.entries().forEach(entry ->
 				System.setProperty(entry.getKey(), entry.getValue())
