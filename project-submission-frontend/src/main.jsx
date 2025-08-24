f674d93c07d348333164071f183fb81bc1e800cb
Hemant <rajugowardipe0@gmail.com>
2025-08-24 11:43:34 +0530

    added vercel web analytics

--- project-submission-frontend/src/main.jsx
+++ project-submission-frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+  <BrowserRouter>
+    <App />
+    <Analytics />   {/* 👈 Added here */}
+  </BrowserRouter>
+)
