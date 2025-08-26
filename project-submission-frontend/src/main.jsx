5e6784f160954e3728a33325af6f98859fba2874
Hemant <rajugowardipe0@gmail.com>
2025-08-27 00:17:48 +0530

    added vercel speed insights

--- project-submission-frontend/src/main.jsx
+++ project-submission-frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
     <Analytics />   {/* 👈 Added here */}
+    <SpeedInsights />
   </BrowserRouter>
 )
