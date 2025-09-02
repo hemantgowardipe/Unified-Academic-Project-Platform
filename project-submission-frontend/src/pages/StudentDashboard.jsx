6a5878004bd45a2a49108af2f05ee5ae00d33801
Hemant <rajugowardipe0@gmail.com>
2025-09-02 23:49:04 +0530

    feat: add PropTypes import to StudentDashboard for improved prop validation

--- project-submission-frontend/src/pages/StudentDashboard.jsx
+++ project-submission-frontend/src/pages/StudentDashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import useDarkMode from "../hooks/useDarkMode";
 import { getTheme } from "../utils/themeConfig";
 import { getMyProjects } from '../services/ProjectService';
