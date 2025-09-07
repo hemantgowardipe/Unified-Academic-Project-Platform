7b96ac6306326bdbb0bbc3c5c554f6d4a47d789d
Hemant <rajugowardipe0@gmail.com>
2025-09-07 14:34:35 +0530

    feat: implement Google Analytics tracking with external script and updated CSP

---
+++ project-submission-frontend/public/ga.js
@@ -0,0 +1,7 @@
+// File: project-submission-frontend/public/ga.js
+// Google Analytics tracking script
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-SHGP5741JX');
